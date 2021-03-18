d443ab207e1970f22f15c022e7e5778a0ddf7665
BeaTea109 <beatea109@hotmail.com>
2021-03-17 19:57:36 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
     UPLOAD_FOLDER = './app/static/uploads'
     SEND_FILE_MAX_AGE_DEFAULT = 0
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@localhost/propertydb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://ecuzjynckcuvwb:8cdc5de3a66b232010aeab7a97bd5ae4c7ef5a3a3528c2f43e6f2a77ac7d1c28@ec2-54-145-102-149.compute-1.amazonaws.com:5432/d206s3rdo2fagi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 
 
